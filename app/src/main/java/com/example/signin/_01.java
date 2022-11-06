69d67ec5f1c7a5e18e4e6266ff54151740723730
weserve <garikapatihemanth9@gmail.com>
2022-11-05 20:01:38 -0400

    Final Commit

---
+++ app/src/main/java/com/example/signin/_01.java
@@ -0,0 +1,13 @@
+package com.example.signin;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class _01 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_01);
+    }
+}
---
+++ app/src/main/java/com/example/signin/_07.java
@@ -0,0 +1,13 @@
+package com.example.signin;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class _07 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_07);
+    }
+}
